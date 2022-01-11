431aa3b60e56f1d091b675af825847ab9d21629f
Nguyen Minh Khoa <nmkhoa89@gmail.com>
2022-01-11 20:57:29 +0700

    Message

--- src/App.js
+++ src/App.js
@@ -11,32 +11,8 @@ const addressZombieFactory = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 // const addressZombieOwnership = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
 // const addressOwnable = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 
-
-function generateZombie(id, name, dna) {
-  let dnaStr = String(dna)
- 
-  while (dnaStr.length < 16)
-    dnaStr = "0" + dnaStr
-
-  let zombieDetails = {
-   
-    headChoice: dnaStr.substring(0, 2) % 7 + 1,
-    
-    eyeChoice: dnaStr.substring(2, 4) % 11 + 1,
-   
-    shirtChoice: dnaStr.substring(4, 6) % 6 + 1,
-    
-    skinColorChoice: parseInt(dnaStr.substring(6, 8) / 100 * 360),
-    eyeColorChoice: parseInt(dnaStr.substring(8, 10) / 100 * 360),
-    clothesColorChoice: parseInt(dnaStr.substring(10, 12) / 100 * 360),
-    zombieName: name,
-    zombieDescription: "A Level 1 CryptoZombie",
-  }
-  return zombieDetails
-}
-
 function App() {
-  var event;
+  
   const [nameZombie, setNameZombie] = useState()
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -52,11 +28,15 @@ function App() {
       const contract = new ethers.Contract(addressZombieFactory, zombiefactory.abi, signer)
       const transaction = await contract.createRandomZombie(nameZombie)
       await transaction.wait()
-       event = await contract.NewZombie(function(error, result) {
-        if (error) return
-        generateZombie(result.zombieId, result.name, result.dna)
-      })
-      console.log(event.zombieDetails)
+       
+       contract.NewZombie().watch({}, '', function(error, result) {
+        if (!error) {
+            console.log("Name " + result.args.name +
+                " ZombieId " + result.args.zombieId +
+                " Dna " + result.args.dna + ".");
+            
+        }
+    })
     }
   }
 
@@ -70,7 +50,7 @@ function App() {
         <input onChange={e => setNameZombie(e.target.value)} placeholder="Name" />
         <button onClick = {newzombie()} > Tạo Một Zombie mới </button>
         <br />
-        <div>{event}</div>
+        
       </header>
     </div>
   );
